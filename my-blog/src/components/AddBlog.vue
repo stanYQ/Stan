5957f7404ef571eda7f7807b9cb0ed04a2516694
StanYQ <724760554@qq.com>
2019-04-02 21:39:09 +0800

    建立 个人 博客项目

---
+++ my-blog/src/components/AddBlog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="add-blog">
+    <h1>AddBlog</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddBlog',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
